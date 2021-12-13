02eb3c64c4be79d29d8bf2396e321fe3fc24f9ca
jinho <rowkaxl@naver.com>
2021-12-13 19:32:45 +0900

    [Add] Green - Dollar class 생성 - times 함수 생성 - amount 프로퍼티 생성

---
+++ Jinho/Currency/Currency/Dollar.swift
@@ -0,0 +1,18 @@
+//
+//  Dollar.swift
+//  Currency
+//
+//  Created by jinho on 2021/12/13.
+//
+
+import Foundation
+
+class Dollar {
+    var amount: Int = 10
+    
+    init(_ multiplier: Int) {
+    }
+    
+    func times(_ amount: Int) {
+    }
+}
