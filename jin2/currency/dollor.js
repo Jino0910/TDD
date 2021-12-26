a6ac04fc093af9a344e0933392ae34323da7d96d
sujin lee <jmakeaz@gmail.com>
2021-12-26 21:57:57 +0900

    Dollar의 amount 인스턴스 변수를 사용하는 코드는 Dollar 자신밖에 없으므로 변수를 private으로 변경

--- jin2/currency/dollor.js
+++ jin2/currency/dollor.js
@@ -1,15 +1,17 @@
 class Dollar {
 
+    #amount;
+
     constructor(amount) {
-        this.amount = amount;
+        this.#amount = amount;
     }
 
     times(multiplier) {
-        return new Dollar(this.amount * multiplier);
+        return new Dollar(this.#amount * multiplier);
     }
 
     isSame(obj) {
-        return this.amount === obj.amount;
+        return this.#amount === obj.#amount;
     }
 }
 
