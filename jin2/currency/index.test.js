bff9e02563daa51bf29b1f7ab4df2e7eaf89f94e
sujin lee <jmakeaz@gmail.com>
2021-12-26 21:48:32 +0900

    Dollar와 Dollar를 비교

--- jin2/currency/index.test.js
+++ jin2/currency/index.test.js
@@ -3,9 +3,9 @@ const Dollar = require('./dollor');
 test('testMultiplication', () => {
     const dollarFive = new Dollar(5);
     let product = dollarFive.times(2);
-    expect(10).toEqual(product.amount);
+    expect(new Dollar(10)).toEqual(product);
     product = dollarFive.times(3);
-    expect(15).toEqual(product.amount);
+    expect(new Dollar(15)).toEqual(product);
 });
 
 test('testEquality', () => {
